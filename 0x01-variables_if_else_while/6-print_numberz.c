ff4da3f0b56c121ee9e22b24e9cdb99d109de8bd
shemomondi <shemomondi746@gmail.com>
2023-03-17 12:46:24 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+	int  d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+}
