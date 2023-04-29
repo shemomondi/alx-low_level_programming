2b73a50c566234087a460a5ad1eb7329fc69c560
shem <shemomondi746@gmail.com>
2023-04-29 06:55:03 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,17 +9,17 @@
  */
 int pop_listint(listint_t **head)
 {
- listint_t *temp;
- int num;
+ listint_t *tem;
+int number;
 
- if (!head || !*head)
- return (0);
+if (!head || !*head)
+return (0);
 
- num = (*head)->n;
- temp = (*head)->next;
- free(*head);
- *head = temp;
+number = (*head)->n;
+tem = (*head)->next;
+free(*head);
+*head = tem;
 
- return (num);
+return (number);
 }
 
